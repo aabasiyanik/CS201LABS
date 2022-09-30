53c4cbd4d0413cac7a959a2a7582be94ff44d2e8
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2022-09-29 20:19:55 -0500

    change

--- src/LAB4/Question1.java
+++ src/LAB4/Question1.java
@@ -1,4 +1,44 @@
 package LAB4;
 
+import java.util.Scanner;
+
 public class Question1 {
+
+    public static double calcu() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("firstNum");;
+        double firstNum = input.nextDouble();
+        System.out.println("secondNum");
+        double secondNum = input.nextDouble();
+        System.out.println("operator");
+        String operator = input.next();
+
+        double result = 0;
+        switch (operator){
+            case "+":
+                result = firstNum+secondNum;
+                break;
+            case "-":
+                result = firstNum-secondNum;
+                break;
+            case "*":
+                result = firstNum*secondNum;
+                break;
+            case "/":
+                result = firstNum/secondNum;
+                break;
+            default:
+                System.out.println("input the correct operator");
+                break;
+
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+
+        System.out.println(calcu());
+
+    }
 }
