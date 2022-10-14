af5987992d28a29d2821526e8420a81fad9254ea
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2022-10-14 13:36:22 -0500

    change

--- src/LAB5/Question2.java
+++ src/LAB5/Question2.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Question2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.println("a");
 
     }
 }
